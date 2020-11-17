44ffb871d1c32bce50b44557187426d12feaaa6a
ApprovedBug <approvedbug@outlook.com>
2020-11-17 21:07:30 +0000

    feat: add base vc and base view

---
+++ APBFoundation/APBFoundation.h
@@ -0,0 +1,18 @@
+//
+//  APBFoundation.h
+//  APBFoundation
+//
+//  Created by ApprovedBug on 17/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APBFoundation.
+FOUNDATION_EXPORT double APBFoundationVersionNumber;
+
+//! Project version string for APBFoundation.
+FOUNDATION_EXPORT const unsigned char APBFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APBFoundation/PublicHeader.h>
+
+
